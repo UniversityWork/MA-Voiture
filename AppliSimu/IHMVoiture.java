8afe2b9afad9605baf68ff86c2bd3dc025a34f86
wladduma@gmail.com <wladdm@hotmail.com>
2015-03-27 09:05:35 +0100

    inital commit

---
+++ AppliSimu/IHMVoiture.java
@@ -0,0 +1,61 @@
+package AppliSimu;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JFrame;
+
+public class IHMVoiture extends JFrame implements Observer{
+
+	private double paramatreConversionMetresPixels = 0.5;
+	private Voiture maVoiture;
+	private CommandeVoiture maCommandeVoiture;
+	
+	private void initGraphique() {
+		this.setTitle("Simulateur de Voiture");
+		this.setSize(505, 505);
+
+		this.maCommandeVoiture = new CommandeVoiture(this, maVoiture);
+
+		this.setVisible(true);
+	}
+	
+	public IHMVoiture(Voiture maVoiture) {
+		super();
+		this.maVoiture = maVoiture;
+		maVoiture.addObserver(this);
+		initGraphique();
+	}
+
+	public IHMVoiture() {
+		super();
+		initGraphique();
+		this.maVoiture = new Voiture(1,1,1);
+	}
+	
+	public int calculerPositionPixels(int xMetres) {
+		return (int) (paramatreConversionMetresPixels * xMetres);	
+	}
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		this.repaint();
+	}
+
+	@Override
+	public void paint(Graphics contexteGraphique) {
+		super.paint(contexteGraphique);
+		contexteGraphique.setColor(Color.red);
+		dessinerVoiture(contexteGraphique);
+	}
+
+
+	private void dessinerVoiture(Graphics contexteGraphique) {
+		int xMetres = maVoiture.getX();
+		int xPixel = calculerPositionPixels(xMetres);
+		contexteGraphique.fillRect(xPixel, 300, 30, 15);
+	}
+	
+}
